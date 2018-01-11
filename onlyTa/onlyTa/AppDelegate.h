ee6628c410d4bb27e638309409b1fe6eebfacaf1
a1anwang <245121415@qq.com>
2018-01-11 15:47:16 +0800

    初始创建
    导入融云SDK 2.8.24

---
+++ onlyTa/onlyTa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  onlyTa
+//
+//  Created by smartwallit on 2017/12/15.
+//  Copyright © 2017年 a1anwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "APPHeader.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
